bc083d4aa4a39b22e795998050af5ee0aaea23c1
doesburg11 <petervandoesburg11@gmail.com>
2023-12-31 20:46:56 +0100

    Gif update

--- pettingzoo/predprey/predprey_31/predprey.py
+++ pettingzoo/predprey/predprey_31/predprey.py
@@ -268,7 +268,7 @@ class PredPrey:
 
         # visualization
         self.screen = None
-        self.save_image_steps = False
+        self.save_image_steps = True
         self.file_name = 0
         self.n_aec_cycles = 0
         # end visualization
