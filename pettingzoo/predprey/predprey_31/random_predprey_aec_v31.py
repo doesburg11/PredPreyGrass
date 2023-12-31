a121d855e3c6171663f088af896ffbad8cf92584
doesburg11 <petervandoesburg11@gmail.com>
2023-12-31 17:35:08 +0100

    update

--- pettingzoo/predprey/predprey_31/random_predprey_aec_v31.py
+++ pettingzoo/predprey/predprey_31/random_predprey_aec_v31.py
@@ -16,6 +16,8 @@ env_kwargs = dict(
     obs_range_prey=7, # must be odd
     action_range=3, # must be odd
     moore_neighborhood_actions=False,
+    energy_loss_per_step_predator = -0.2,
+    energy_loss_per_step_prey = -0.1,     
     pixel_scale=40
 )
 
--- pettingzoo/predprey/predprey_31/sb3_predprey_vector_v31.py
+++ pettingzoo/predprey/predprey_31/sb3_predprey_vector_v31.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     eval_and_watch_model = True
     training_steps_string = "10_000_000"
     training_steps = int(training_steps_string)
-    loaded_policy = "./trained_models/predprey/predprey_2023-12-31_15:12/predprey_steps_10_000_000.zip"
+    loaded_policy = "./trained_models/predprey/predprey_2023-12-31_16:21/predprey_steps_10_000_000.zip"
     env_kwargs = dict(
         max_cycles=100000, 
         x_grid_size=16, 
@@ -148,6 +148,8 @@ if __name__ == "__main__":
         obs_range_prey=7, # must be odd
         action_range=3, # must be odd
         moore_neighborhood_actions=False,
+        energy_loss_per_step_predator = -0.2,
+        energy_loss_per_step_prey = -0.1,     
         pixel_scale=40
         )
 
