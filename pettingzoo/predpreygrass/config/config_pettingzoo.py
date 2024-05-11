f2a570dbfa9866f03c1b276e9151b3f1330592d9
doesburg11 <petervandoesburg11@gmail.com>
2024-05-11 20:34:25 +0200

    update readme

--- pettingzoo/predpreygrass/config/config_pettingzoo.py
+++ pettingzoo/predpreygrass/config/config_pettingzoo.py
@@ -1,7 +1,7 @@
 # put in here your own directory to the output folder
 local_output_directory = "/home/doesburg/Dropbox/02_marl_results/predpreygras_results/"
 
-training_steps_string = "_000_000"
+training_steps_string = "10_000_000"
 
 env_kwargs = dict(
     # environment parameters
@@ -14,12 +14,12 @@ env_kwargs = dict(
     n_possible_grass=30,
     n_initial_active_predator=6,
     n_initial_active_prey=8,
-    
+    # observation parameters
     max_observation_range=7, # must be odd and not smaller than any obs_range
     obs_range_predator=5, # must be odd    
     obs_range_prey=7, # must be odd
     # energy parameters
-    energy_gain_per_step_predator = -0.3,
+    energy_gain_per_step_predator = -0.1,
     energy_gain_per_step_prey = -0.05,
     energy_gain_per_step_grass = 0.2,  
     catch_prey_energy = 5.0,
@@ -35,14 +35,14 @@ env_kwargs = dict(
     catch_reward_prey = 0.0,  
     death_reward_prey = 0.0,
     death_reward_predator = 0.0,
-    reproduction_reward_prey = 10.0,
-    reproduction_reward_predator = 10.0,
+    reproduction_reward_prey = 11.0,  # in three cycles attainable
+    reproduction_reward_predator = 15.0, # in three cycles attainable
     # create agents parameters
     regrow_grass=True,
     create_prey = True,
     create_predator = True, 
-    prey_creation_energy_threshold = 8.0,
-    predator_creation_energy_threshold = 10.0,
+    prey_creation_energy_threshold = 20.0,
+    predator_creation_energy_threshold = 20.0,
     
     # visualization parameters
     cell_scale=40,
