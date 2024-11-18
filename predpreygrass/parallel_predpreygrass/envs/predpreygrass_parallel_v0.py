b17b1dccdd35f774fca26300245cafd70f5db6fa
doesburg11 <petervandoesburg11@gmail.com>
2024-11-18 13:26:55 +0100

    cleanup

--- predpreygrass/parallel_predpreygrass/envs/predpreygrass_parallel_v0.py
+++
@@ -1,3 +0,0 @@
-from predpreygrass.parallel_predpreygrass.envs.base_env.predpreygrass_parallel import env, parallel_env, raw_env
-
-__all__ = ["env", "parallel_env", "raw_env"]
--- predpreygrass/predpreygrass/envs/predpreygrass_aec_v0.py
+++
@@ -1,3 +0,0 @@
-from predpreygrass.predpreygrass.envs.base_env.predpreygrass_aec import env, parallel_env, raw_env
-
-__all__ = ["env", "parallel_env", "raw_env"]
