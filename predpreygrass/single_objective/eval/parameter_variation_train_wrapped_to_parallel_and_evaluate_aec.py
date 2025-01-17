e38b3ce6fd9f2e04c86b602364036f35cb16b926
doesburg11 <petervandoesburg11@gmail.com>
2025-01-18 00:13:03 +0100

    update

--- predpreygrass/single_objective/eval/parameter_variation_train_wrapped_to_parallel_and_evaluate_aec.py
+++ predpreygrass/single_objective/eval/parameter_variation_train_wrapped_to_parallel_and_evaluate_aec.py
@@ -15,7 +15,7 @@ from os.path import dirname as up
 
 if __name__ == "__main__":
     parameter_variation_parameter_string = "random_action_prob"
-    parameter_variation_scenarios = [0.30, 0.35, 0.40]
+    parameter_variation_scenarios = [0.45, 0.50]
 
     time_stamp_string = str(time.strftime("%Y-%m-%d_%H:%M:%S"))
     env_fn = predpreygrass_aec_v0
