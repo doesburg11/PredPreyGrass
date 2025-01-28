a2bbd0f77e346fce865a31dcfc1d232351154c45
doesburg11 <petervandoesburg11@gmail.com>
2025-01-28 21:28:58 +0100

    delete parallel_env random policy

--- predpreygrass/single_objective/eval/evaluate_random_policy_to_file_aec_env.py
+++ predpreygrass/single_objective/eval/evaluate_random_policy_to_file_aec_env.py
@@ -1,7 +1,5 @@
 # discretionary libraries
-from predpreygrass.single_objective.envs import (
-    predpreygrass_aec_v0,
-)
+from predpreygrass.single_objective.envs import predpreygrass_aec_v0
 from predpreygrass.single_objective.config.config_predpreygrass import (
     env_kwargs,
     local_output_root,
