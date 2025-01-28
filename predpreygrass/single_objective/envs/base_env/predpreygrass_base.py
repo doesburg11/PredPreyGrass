0f7d475c8deb86615446bbd232ef07a3e4e19272
doesburg11 <petervandoesburg11@gmail.com>
2025-01-28 21:08:53 +0100

    delete parallel_env

--- predpreygrass/single_objective/envs/base_env/predpreygrass_base.py
+++ predpreygrass/single_objective/envs/base_env/predpreygrass_base.py
@@ -1,9 +1,6 @@
 # discretionary libraries
 from .predpreygrass_super_base import PredPreyGrassSuperBaseEnv
 
-# external libraries
-from typing import Dict
-
 
 class PredPreyGrassBaseEnv(PredPreyGrassSuperBaseEnv):
 
@@ -11,15 +8,15 @@ class PredPreyGrassBaseEnv(PredPreyGrassSuperBaseEnv):
         self._reset_rewards()
 
         # apply rules of engagement for all agents
-        for predator_instance in self.active_agent_instance_list_type[self.predator_type_nr][:]:  
+        for predator_instance in self.active_agent_instance_list_type[
+            self.predator_type_nr
+        ][:]:
             if predator_instance.energy > 0:
                 # new is the position of the predator after the move
                 x_new, y_new = predator_instance.position
-                prey_instance_in_predator_cell = (
-                    self.agent_instance_in_grid_location[self.prey_type_nr][
-                        (x_new, y_new)
-                    ]
-                )
+                prey_instance_in_predator_cell = self.agent_instance_in_grid_location[
+                    self.prey_type_nr
+                ][(x_new, y_new)]
                 if prey_instance_in_predator_cell is not None:
                     predator_instance.energy += prey_instance_in_predator_cell.energy
                     self._deactivate_agent(prey_instance_in_predator_cell)
@@ -52,16 +49,19 @@ class PredPreyGrassBaseEnv(PredPreyGrassSuperBaseEnv):
             else:  # predator_instance.energy <= 0
                 self._deactivate_agent(predator_instance)
                 self.n_active_agent_type[self.predator_type_nr] -= 1
-                self.agent_age_of_death_list_type[self.predator_type_nr].append(predator_instance.age)
+                self.agent_age_of_death_list_type[self.predator_type_nr].append(
+                    predator_instance.age
+                )
                 self.n_starved_predator += 1
                 self.rewards[predator_instance.agent_name] += self.death_reward_predator
 
-        for prey_instance in self.active_agent_instance_list_type[self.prey_type_nr][:]:  
+        for prey_instance in self.active_agent_instance_list_type[self.prey_type_nr][:]:
             if prey_instance.energy > 0:
                 # new is the position of the prey after the move
                 x_new, y_new = prey_instance.position
                 grass_instance_in_prey_cell = self.agent_instance_in_grid_location[
-                    self.grass_type_nr][(x_new,y_new)]
+                    self.grass_type_nr
+                ][(x_new, y_new)]
                 if grass_instance_in_prey_cell is not None:
                     prey_instance.energy += grass_instance_in_prey_cell.energy
                     self._deactivate_agent(grass_instance_in_prey_cell)
@@ -82,10 +82,12 @@ class PredPreyGrassBaseEnv(PredPreyGrassSuperBaseEnv):
                         self.rewards[
                             prey_instance.agent_name
                         ] += self.reproduction_reward_prey
-            else:  
+            else:
                 self._deactivate_agent(prey_instance)
                 self.n_active_agent_type[self.prey_type_nr] -= 1
-                self.agent_age_of_death_list_type[self.prey_type_nr].append(prey_instance.age)
+                self.agent_age_of_death_list_type[self.prey_type_nr].append(
+                    prey_instance.age
+                )
                 self.n_starved_prey += 1
                 self.rewards[prey_instance.agent_name] += self.death_reward_prey
 
@@ -114,42 +116,20 @@ class PredPreyGrassBaseEnv(PredPreyGrassSuperBaseEnv):
         self.n_cycles += 1
         self._record_population_metrics()
 
+
 class PredPreyGrassAECEnv(PredPreyGrassBaseEnv):
     """
-    Pred/Prey/Grass PettingZoo multi-agent learning AEC environment. This environment 
+    Pred/Prey/Grass PettingZoo multi-agent learning AEC environment. This environment
     transfers the energy of eaten prey/grass to the predator/prey while the grass
     regrows over time. The environment is a 2D grid world where agents can move
     in four cardinal directions.
     """
-  
+
     def step(self, action, agent_instance, is_last_step_of_cycle):
-        
+
         if agent_instance.is_active:
             self._apply_agent_action(agent_instance, action)
 
         # apply the engagement rules and reap rewards
         if is_last_step_of_cycle:
             self.process_engagements_agent()
-
-class PredPreyGrassParallelEnv(PredPreyGrassBaseEnv):
-    """
-    Pred/Prey/Grass PettingZoo multi-agent learning Parallel environment. This environment 
-    transfers the energy of eaten prey/grass to the predator/prey while the grass
-    regrows over time. The environment is a 2D grid world where agents can move
-    in four cardinal directions. 
-    """
-   
-    def step(self, actions: Dict[str, int]) -> None:
-        """
-        Executes a step in the environment based on the provided actions.
-
-        Parameters:
-            actions (Dict[str, int]): Dictionary containing actions for each agent.
-        """
-        # 1] apply actions (i.e. movements) for all active agents in parallel
-        for predator_instance in self.active_agent_instance_list_type[self.predator_type_nr]:
-            self._apply_agent_action(predator_instance, actions[predator_instance.agent_name])
-        for prey_instance in self.active_agent_instance_list_type[self.prey_type_nr]:
-            self._apply_agent_action(prey_instance, actions[prey_instance.agent_name])
-
-        self.process_engagements_agent()
