933cd68e027cba169dd442f368c89d8904d2197b
doesburg11 <petervandoesburg11@gmail.com>
2025-04-08 23:33:04 +0200

    Adjust environment runner configuration for improved resource allocation

--- src/predpreygrass/rllib/v4_select_coef_HBP/train_rllib_ppo_multiagentenv_resources.py
+++ src/predpreygrass/rllib/v4_select_coef_HBP/train_rllib_ppo_multiagentenv_resources.py
@@ -33,11 +33,11 @@ num_gpus_per_learner=1
 num_learners=1
 
 # env_runners
-num_env_runners=7
-num_envs_per_env_runner=4
+num_env_runners=8
+num_envs_per_env_runner=3
 rollout_fragment_length="auto"
 sample_timeout_s=600
-num_cpus_per_env_runner=4 
+num_cpus_per_env_runner=3 
 
 #resources
 num_cpus_for_main_process=4
