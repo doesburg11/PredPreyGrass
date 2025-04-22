a936ed2b173891039a68b3f382063538c022e72a
doesburg11 <petervandoesburg11@gmail.com>
2025-04-22 09:11:32 +0200

    Update checkpoint paths for evaluation with new predator observation range

--- src/predpreygrass/rllib/v5_move_energy/evaluate_ppo_from_checkpoint.py
+++ src/predpreygrass/rllib/v5_move_energy/evaluate_ppo_from_checkpoint.py
@@ -50,8 +50,8 @@ def policy_mapping_fn(agent_id, *args, **kwargs):
 ray_results_dir = '/home/doesburg/Dropbox/02_marl_results/predpreygrass_results/ray_results'
 #checkpoint_root = '/v5_move_energy/pred_obs_range/Pred_11_Prey_9/PPO_PredPreyGrass_109fe_00000_0_2025-04-19_10-41-19/'
 #checkpoint_root = '/v5_move_energy/reward_1.0/obs_range_Pred_11_Prey_9/PPO_PredPreyGrass_109fe_00000_0_2025-04-19_10-41-19/'
-checkpoint_root = '/v5_move_energy/reward_10.0/obs_range_Pred_7_Prey_9/PPO_PredPreyGrass_aa713_00000_0_2025-04-19_20-25-26/'
-checkpoint_dir = 'checkpoint_000084'
+checkpoint_root = '/v5_move_energy/reward_10.0/obs_range_Pred_13_Prey_9/PPO_PredPreyGrass_63d5d_00000_0_2025-04-21_14-16-01/'
+checkpoint_dir = 'checkpoint_000080'
 checkpoint_path = os.path.abspath(ray_results_dir + checkpoint_root+ checkpoint_dir)
 # === Get training directory and prepare eval output dir ===
 training_dir = os.path.dirname(os.path.dirname(checkpoint_path))
