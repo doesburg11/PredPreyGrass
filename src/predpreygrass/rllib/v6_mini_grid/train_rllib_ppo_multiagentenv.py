ae948a7eeb664d56a564e10ff54050d1b220fa69
doesburg11 <petervandoesburg11@gmail.com>
2025-04-23 22:48:02 +0200

    Update existing experiment directory name for PPO results

--- src/predpreygrass/rllib/v6_mini_grid/train_rllib_ppo_multiagentenv.py
+++ src/predpreygrass/rllib/v6_mini_grid/train_rllib_ppo_multiagentenv.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     register_env("PredPreyGrass", env_creator)
     ray_results_dir = "~/Dropbox/02_marl_results/predpreygrass_results/ray_results/"
     ray_results_path = Path(ray_results_dir).expanduser()
-    existing_experiment_dir = "PPO_2025-04-10_15-12-05"
+    existing_experiment_dir = "PPO_2025-04-23_21-51-36"
     experiment_path = ray_results_path / existing_experiment_dir
     if (experiment_path / "tuner.pkl").exists():
         restored_tuner = tune.Tuner.restore(
