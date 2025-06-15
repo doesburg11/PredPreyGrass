53f2860f7494d7833013ebc5d622db7869cb129e
doesburg11 <petervandoesburg11@gmail.com>
2025-06-15 19:38:48 +0200

    Fix indentation of sys.path append statement for clarity

--- src/predpreygrass/pettingzoo/eval/evaluate_ppo_from_file_aec_env.py
+++ src/predpreygrass/pettingzoo/eval/evaluate_ppo_from_file_aec_env.py
@@ -19,12 +19,12 @@ import os
 import sys
 from os.path import dirname as up
 
-# Add the parent directory to sys.path
-sys.path.append(up(up(__file__)))
-
 from predpreygrass.pettingzoo.envs import predpreygrass_aec_v0
 from predpreygrass.pettingzoo.config.config_predpreygrass import env_kwargs, training_steps_string
 from predpreygrass.pettingzoo.eval.utils.evaluator import Evaluator
+
+# Add the parent directory to sys.path
+sys.path.append(up(up(__file__)))
 
 
 if __name__ == "__main__":
