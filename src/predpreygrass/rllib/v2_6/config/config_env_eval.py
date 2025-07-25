23d8db5fade42a1bd499bc152ddee6dfc7e48fa4
doesburg11 <petervandoesburg11@gmail.com>
2025-07-25 11:59:42 +0200

    fix: update environment configuration parameters for evaluation

--- src/predpreygrass/rllib/v2_6/config/config_env_eval.py
+++ src/predpreygrass/rllib/v2_6/config/config_env_eval.py
@@ -2,22 +2,13 @@ from .config_env_train import config_env
 
 config_env = {
     **config_env,
-    "grid_size": 60,
-    "type_1_action_range": 3,
-    "type_2_action_range": 3,
-    "reward_prey_eat_grass": {
-        "type_1_prey": 0.0,
-        "type_2_prey": 3.0,
-    },
-    "n_possible_type_1_predators": 100,
+    "grid_size": 25,
+    "n_possible_type_1_predators": 50,
     "n_possible_type_2_predators": 0,
-    "n_possible_type_1_prey": 150,
-    "n_possible_type_2_prey": 150,
+    "n_possible_type_1_prey": 50,
+    "n_possible_type_2_prey": 50,
     "n_initial_active_type_1_predator": 15,
     "n_initial_active_type_2_predator": 0,
-    "n_initial_active_type_1_prey": 5,
-    "n_initial_active_type_2_prey": 5,
-    "mutation_rate_predator": 0.0,
-    "mutation_rate_prey": 0.0,
-    "move_energy_cost_factor": 0.00,  # Reduced to zero for testing
+    "n_initial_active_type_1_prey": 10,
+    "n_initial_active_type_2_prey": 10,
 }
