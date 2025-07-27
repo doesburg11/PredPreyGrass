4ff7ad9b55a57936d6cbd6c5693e46727a9e7883
doesburg11 <petervandoesburg11@gmail.com>
2025-07-27 23:53:21 +0200

    fix: update movement energy cost and mutation rates in environment configuration

--- src/predpreygrass/rllib/v2_7/config/config_env_train.py
+++ src/predpreygrass/rllib/v2_7/config/config_env_train.py
@@ -7,7 +7,7 @@ config_env = {
     # Energy settings
     "energy_loss_per_step_predator": 0.15,
     "energy_loss_per_step_prey": 0.05,
-    "move_energy_cost_factor": 0.0,
+    "move_energy_cost_factor": 0.0025,
     "initial_energy_predator": 5.0,
     # Learning agents
     "n_possible_type_1_predators": 30,
@@ -15,8 +15,8 @@ config_env = {
     "n_initial_active_type_1_predator": 10,
     "n_initial_active_type_2_predator": 10,
     # mutation settings
-    "mutation_rate_predator": 0.0,  # No mutations for this environment
-    "mutation_rate_prey": 0.0,  # No mutations for this environment
+    "mutation_rate_predator": 0.05,  # No mutations for this environment
+    "mutation_rate_prey": 0.05,  # No mutations for this environment
     # Grass settings
     "initial_num_grass": 50,
     "energy_gain_per_step_grass": 0.08,
