2e0d86bf26f57c96099504d16bfede4d6118743b
doesburg11 <petervandoesburg11@gmail.com>
2025-07-28 23:07:33 +0200

    fix: update environment configuration for evaluation with new initial predator and prey counts

--- src/predpreygrass/rllib/v2_7/config/config_env_eval.py
+++ src/predpreygrass/rllib/v2_7/config/config_env_eval.py
@@ -2,13 +2,9 @@ from .config_env_train import config_env
 
 config_env = {
     **config_env,
-    "grid_size": 25,
-    "n_possible_type_1_predators": 30,
-    "n_possible_type_2_predators": 30,
-    "n_possible_type_1_prey": 40,
-    "n_possible_type_2_prey": 40,
-    "n_initial_active_type_1_predator": 10,
-    "n_initial_active_type_2_predator": 10,
-    "n_initial_active_type_1_prey": 10,
-    "n_initial_active_type_2_prey": 10,
+    "max_steps": 2000,
+    "n_initial_active_type_1_predator": 20,
+    "n_initial_active_type_2_predator": 0,
+    "n_initial_active_type_1_prey": 20,
+    "n_initial_active_type_2_prey": 0,
 }
