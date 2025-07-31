1bb67cd02ad1ab8bc68e49e7f68c4e402b42d9b9
doesburg11 <petervandoesburg11@gmail.com>
2025-07-31 23:58:13 +0200

    feat: add configuration files for environment and PPO training settings

---
+++ src/predpreygrass/rllib/v2_7/config/config_ppo_gpu_test.py
@@ -0,0 +1,17 @@
+config_ppo = {
+    # Training
+    "train_batch_size": 16000,
+    "gamma": 0.99,
+    "lr": 0.0003,
+    # Learners
+    "num_gpus_per_learner": 1,
+    "num_learners": 1,
+    # Env runners
+    "num_env_runners": 4,
+    "num_envs_per_env_runner": 8,
+    "rollout_fragment_length": 400,
+    "sample_timeout_s": 600,
+    "num_cpus_per_env_runner": 1,
+    # Resources
+    "num_cpus_for_main_process": 4,
+}
