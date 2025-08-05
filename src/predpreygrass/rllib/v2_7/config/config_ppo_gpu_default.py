04c58f783992d39ec54f5111be86d239d149c7a5
doesburg11 <petervandoesburg11@gmail.com>
2025-08-05 21:05:56 +0200

    fix: update num_epochs in PPO configuration from 10 to 5

--- src/predpreygrass/rllib/v2_7/config/config_ppo_gpu_default.py
+++ src/predpreygrass/rllib/v2_7/config/config_ppo_gpu_default.py
@@ -6,7 +6,7 @@ config_ppo = {
     "lambda_": 1.0,
     "train_batch_size_per_learner": 1024,
     "minibatch_size": 128,
-    "num_epochs": 10,
+    "num_epochs": 5,
     "entropy_coeff": 0.0,
     "vf_loss_coeff": 1.0,
     "clip_param": 0.3,
