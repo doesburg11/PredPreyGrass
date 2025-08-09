967ef014a71f8c4da72c77fa43bb861f731ff821
doesburg11 <petervandoesburg11@gmail.com>
2025-08-09 16:46:45 +0200

    refactor: update learning rate and adjust environment runner configuration for improved training setup

--- src/predpreygrass/rllib/v2_7/config/config_ppo_cpu_pbt.py
+++ src/predpreygrass/rllib/v2_7/config/config_ppo_cpu_pbt.py
@@ -1,7 +1,7 @@
 config_ppo = {
     "max_iters": 10,
     # Core learning
-    "lr": 5e-5,
+    "lr": 1e-4,
     "gamma": 0.99,
     "lambda_": 0.95,
     "train_batch_size_per_learner": 1024,
@@ -12,14 +12,14 @@ config_ppo = {
     "clip_param": 0.3,
     # Resources
     "num_learners": 1,
-    "num_env_runners": 28,
+    "num_env_runners": 2,
     "num_envs_per_env_runner": 1,
-    "num_gpus_per_learner": 1,
+    "num_gpus_per_learner": 0,
     "num_cpus_for_main_process": 1,
     "num_cpus_per_env_runner": 1,
     "sample_timeout_s": 600,
     "rollout_fragment_length": "auto",
-    # KL / exploration
+    # KL / explorationclear
     "kl_coeff": 0.2,
     "kl_target": 0.01,
 }
