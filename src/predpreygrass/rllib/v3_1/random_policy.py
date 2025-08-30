d635974249471e015be52919bccdeb97d94baa28
doesburg11 <petervandoesburg11@gmail.com>
2025-08-30 22:21:27 +0200

    fix: update import paths to use v3_1 in random_policy.py

--- src/predpreygrass/rllib/v3_1/random_policy.py
+++ src/predpreygrass/rllib/v3_1/random_policy.py
@@ -4,9 +4,9 @@ No backward stepping is implemented in this version,
 because that is pointless for debugging and testing
 with a random policy.
 """
-from predpreygrass.rllib.v3_0.predpreygrass_rllib_env import PredPreyGrass
-from predpreygrass.rllib.v3_0.config.config_env_random import config_env
-from predpreygrass.rllib.v3_0.utils.pygame_grid_renderer_rllib import PyGameRenderer
+from predpreygrass.rllib.v3_1.predpreygrass_rllib_env import PredPreyGrass
+from predpreygrass.rllib.v3_1.config.config_env_random import config_env
+from predpreygrass.rllib.v3_1.utils.pygame_grid_renderer_rllib import PyGameRenderer
 
 # external libraries
 import pygame
