17c215f1e2c692998531e0f2f93945e5fab0b34f
doesburg11 <petervandoesburg11@gmail.com>
2025-09-09 15:11:40 +0200

    feat: add configuration for PPO HBP best and search

---
+++ src/predpreygrass/rllib/hyper_parameter_tuning/config/config_ppo_hbp_search.py
@@ -0,0 +1,26 @@
+config_ppo = {
+    "max_iters": 500,
+    # Core learning
+    "lr": 0.0003,
+    "gamma": 0.99,
+    "lambda_": 1.0,
+    "train_batch_size_per_learner": 1024,
+    "minibatch_size": 128,
+    "num_epochs": 30,
+    "entropy_coeff": 0.0,
+    "vf_loss_coeff": 1.0,
+    "clip_param": 0.3,
+    # Resources
+    "num_learners": 1,
+    "num_env_runners": 6,
+    "num_envs_per_env_runner": 1,
+    "num_gpus_per_learner": 0,
+    "num_cpus_per_learner": 1,
+    "num_cpus_for_main_process": 1,
+    "num_cpus_per_env_runner": 1,
+    "sample_timeout_s": 600,
+    "rollout_fragment_length": "auto",
+    # KL / exploration
+    "kl_coeff": 0.2,
+    "kl_target": 0.01,
+}
