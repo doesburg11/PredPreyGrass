d409294f1ae1b2d7c19ee7b1577755cf12f31693
doesburg11 <petervandoesburg11@gmail.com>
2025-11-02 22:42:46 +0100

    Update experiment name format in PPO training script

--- src/predpreygrass/rllib/walls_occlusion_factored_reset/tune_ppo_walls_proper_termination_factored_reset.py
+++ src/predpreygrass/rllib/walls_occlusion_factored_reset/tune_ppo_walls_proper_termination_factored_reset.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     ray_results_dir = "~/Dropbox/02_marl_results/predpreygrass_results/ray_results/"
     ray_results_path = Path(ray_results_dir).expanduser()
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    experiment_name = f"PPO_PROPER_TERMINATION_REFACTORED_RESET_{timestamp}"
+    experiment_name = f"PPO_REFACTORED_RESET_{timestamp}"
     experiment_path = ray_results_path / experiment_name
     experiment_path.mkdir(parents=True, exist_ok=True)
 
