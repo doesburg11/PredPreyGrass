a94fbda7d816d96de4f0f6cef5de3389456ae748
doesburg11 <petervandoesburg11@gmail.com>
2025-11-17 09:24:13 +0100

    kin_kick_back

--- src/predpreygrass/rllib/rllib_termination/predpreygrass_rllib_env_works_3.py
+++ src/predpreygrass/rllib/rllib_termination/predpreygrass_rllib_env_works_3.py
@@ -354,10 +354,9 @@ class PredPreyGrass(MultiAgentEnv):
         self.truncations["__all__"] = False
         self.infos = {aid: self._pending_infos.get(aid, {}) for aid in all_ids}
         step_data = {}
+        kin_reward = 1.0
         alive_agents = set(self.agents)
         for agent in self.agents:
-            kin_reward = self._get_type_specific("kin_kick_back_predator", agent) \
-                if "predator" in agent else self._get_type_specific("kin_kick_back_prey", agent)
             pos = self.agent_positions[agent]
             energy = self.agent_energies[agent]
             deltas = self._per_agent_step_deltas.get(agent, {"decay": 0.0, "move": 0.0, "eat": 0.0, "repro": 0.0})
