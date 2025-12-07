4f1d2cd7eea52d50a7f346a3e1f3db1b48f67612
doesburg11 <petervandoesburg11@gmail.com>
2025-12-07 10:54:58 +0100

    Update checkpoint paths for PPO reproduction reward evaluation

--- src/predpreygrass/rllib/lineage_rewards/evaluate_ppo_from_checkpoint_debug.py
+++ src/predpreygrass/rllib/lineage_rewards/evaluate_ppo_from_checkpoint_debug.py
@@ -123,8 +123,8 @@ def policy_pi(observation, policy_module, deterministic=True):
 def setup_environment_and_visualizer(now):
 
     ray_results_dir = "/home/doesburg/Dropbox/02_marl_results/predpreygrass_results/ray_results/"
-    checkpoint_root = "PPO_REPRODUCTION_REWARD_LINEAGE_REWARDS_LIMITED_FERTILITY_AGE_2025-12-04_17-50-39/PPO_PredPreyGrass_5d2d7_00000_0_2025-12-04_17-50-39/"
-    checkpoint_dir = "checkpoint_000025"
+    checkpoint_root = "PPO_REPRODUCTION_REWARD_NO_LINEAGE_REWARDS_DEFAULT_CONFIG_PRED_DECAY_0_18_NUM_GRASS_120_2025-12-06_23-11-43/PPO_PredPreyGrass_8c291_00000_0_2025-12-06_23-11-43/"
+    checkpoint_dir = "checkpoint_000009"
   
     checkpoint_path = os.path.join(ray_results_dir, checkpoint_root, checkpoint_dir)
     # training_dir = os.path.dirname(checkpoint_path)
