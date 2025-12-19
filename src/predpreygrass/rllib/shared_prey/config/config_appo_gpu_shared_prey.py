af3ea3b1df3de0e51240c8d6feec7e21ff800668
doesburg11 <petervandoesburg11@gmail.com>
2025-12-19 22:59:11 +0100

    Add configuration files for APPO training with CPU and GPU support

---
+++ src/predpreygrass/rllib/shared_prey/config/config_appo_gpu_shared_prey.py
@@ -0,0 +1,25 @@
+config_appo = {
+    "max_iters": 1000,
+    # Core learning
+    "lr": 3e-4,
+    "gamma": 0.99,
+    "train_batch_size_per_learner": 4096,
+    "entropy_coeff": 0.01,
+    "vf_loss_coeff": 0.5,
+    "clip_param": 0.3,
+    "kl_coeff": 0.2,
+    "kl_target": 0.01,
+    "use_kl_loss": True,
+    "vtrace": True,
+    "vf_clip_param": 10.0,
+    "grad_clip": 40.0,
+    # Resources
+    "num_learners": 1,
+    "num_env_runners": 4,
+    "num_envs_per_env_runner": 2,
+    "num_gpus_per_learner": 1,
+    "num_cpus_for_main_process": 4,
+    "num_cpus_per_env_runner": 4,
+    "sample_timeout_s": 600,
+    "rollout_fragment_length": 100,
+}
