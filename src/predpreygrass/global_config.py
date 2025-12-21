fde7408b8bbbd57b4f661693f83dbaa90024545e
doesburg11 <petervandoesburg11@gmail.com>
2025-12-21 10:41:31 +0100

    update

---
+++ src/predpreygrass/global_config.py
@@ -0,0 +1,5 @@
+import os
+
+# put in here your own directory to the output folder
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+RESULTS_DIR = os.path.join(PROJECT_PATH, "output")
